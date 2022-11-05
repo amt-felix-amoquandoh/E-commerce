f5828725e44061129223892c0e64e9972cffc576
Felix Ekow Amoquandoh <felix.amoquandoh@amalitech.org>
2022-11-05 18:32:38 +0000

    app files update

--- js/script.js
+++ js/script.js
@@ -1,21 +1,25 @@
  const mobileMenu = document.getElementById("mobileMenu");
  const navMenuItems = document.getElementById("navItems");
  const navMenuClose = document.getElementById("mobileMenuClose");
+ const shopICon = document.querySelector(".shopIcon");
 
 
  if (mobileMenu) {
     mobileMenu.addEventListener("click", () => {
         navMenuItems.classList.add("active");
-        mobileMenu.style.display = "none"
-        navMenuClose.style.display = "flex"
-        // navMenuClose.addEventListener("click", () => {
-        //     navMenuItems.classList.add("hide");
-        //     navMenuClose.style.display = "none"
-        //     mobileMenu.style.display = "flex"
-        // })
+        mobileMenu.style.display = "none";
+        shopICon.style.display = "none";
     })    
  }
 
+
+ if (navMenuClose) {
+  navMenuClose.addEventListener("click", () => {
+       navMenuItems.classList.remove("active");
+       mobileMenu.style.display = "flex" 
+       shopICon.style.display = "flex";      
+     })
+ }
 
 
 
