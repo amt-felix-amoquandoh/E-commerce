2d31d8741132ec30bbc9eb91475179e208c37606
Felix Ekow Amoquandoh <105642899+felixEKow@users.noreply.github.com>
2023-01-09 16:35:29 +0000

    project update

--- js/order.js
+++ js/order.js
@@ -7,21 +7,21 @@ const shopItems = [
     title: "Classic Fit Suit",
     description: "Classic, best suits man with a fuller build",
     price: "Ghc 1899",
-    uniqueId: "classic",
+    uniqueId: "classic"
   },
   {
     image: "../img/homeCart/image2.png",
     title: "Slim Fit Suit",
     description: "Contoured to the body, slim but has some room for comfort",
     price: "Ghc 1960",
-    uniqueId: "slim",
+    uniqueId: "slim"
   },
   {
     image: "../img/homeCart/image3.png",
     title: "Modern Fit Suit",
     description: "Slim fit, contoured, shorter length.",
     price: "Ghc 2899",
-    uniqueId: "modern",
+    uniqueId: "modern"
   },
   {
     image: "../img/homeCart/image4.png",
@@ -29,21 +29,21 @@ const shopItems = [
     description:
       "Popular style from the 80s where the classic ‘V’ style was a mainstay",
     price: "Ghc 2099",
-    uniqueId: "patchpocket",
+    uniqueId: "patchpocket"
   },
   {
     image: "../img/homeCart/image5.png",
     title: "Double Breasted Suit",
     description: "4, 6 or 8 buttons max, 6 being the most common",
     price: "Ghc 2800",
-    uniqueId: "doublebreasted",
+    uniqueId: "doublebreasted"
   },
   {
     image: "../img/homeCart/image6.png",
     title: "Single Breasted Suit",
     description: "Inclusion of either one, two or three buttons along the seam",
     price: "Ghc 1999",
-    uniqueId: "singlebreasted",
+    uniqueId: "singlebreasted"
   },
   {
     image: "../img/homeCart/image7.png",
@@ -51,7 +51,7 @@ const shopItems = [
     description:
       "Noticeable high peaks directed to shoulders, most popular style from the 16th century.",
     price: "Ghc 2444",
-    uniqueId: "peak",
+    uniqueId: "peak"
   },
   {
     image: "../img/homeCart/image8.png",
@@ -59,61 +59,61 @@ const shopItems = [
     description:
       "Rounded sides, continuous curve with no hard edges, iconic smoking jacket style.",
     price: "Ghc 2223",
-    uniqueId: "shawl",
+    uniqueId: "shawl"
   },
   {
     image: "../img/homeCart/image9.png",
     title: "Other clothings",
     description: "Classic, best suits man with a fuller build",
     price: "Ghc 899",
-    uniqueId: "tshirts",
+    uniqueId: "tshirts"
   },
   {
     image: "../img/homeCart/image10.png",
     title: "Other clothings",
     description: "Classic, best suits man with a fuller build",
     price: "Ghc 899",
-    uniqueId: "longsleeves",
+    uniqueId: "longsleeves"
   },
   {
     image: "../img/homeCart/image11.png",
     title: "Other clothings",
     description: "Classic, best suits man with a fuller build",
     price: "Ghc 899",
-    uniqueId: "shortsleeves",
+    uniqueId: "shortsleeves"
   },
   {
     image: "../img/homeCart/image13.png",
     title: "Other clothings",
     description: "Classic, best suits man with a fuller build",
     price: "Ghc 899",
-    uniqueId: "womensjacket",
-  },
+    uniqueId: "womensjacket"
+  }
 ];
 
 let cartBasket = JSON.parse(localStorage.getItem("suits")) || [];
-
-const itemsInCart = () => {
-    let cartIcon = document.getElementById("itemsUpdate");
-    cartIcon.innerHTML = cartBasket.map((idx) => idx.quantity).reduce((x, y) => x + y, 0);
-  }
-  
-  const itemsMobileInCart = () => {
-    let cartIcon = document.getElementById("mobileItemsUpdate");
-    cartIcon.innerHTML = cartBasket.map((idx) => idx.quantity).reduce((x, y) => x + y, 0);
-  }
-  
-  itemsInCart();
-  itemsMobileInCart();
   
 
-let loadCartItems = () => {
+const loadCartItems = () => {
  if (cartBasket.length !== 0) {
   return (ordersInCart.innerHTML = cartBasket.map((idx) => {
-    let {id , item} = idx;
-    // let findItems = shopItems.find((stuff) => stuff.id === id) || [];
+    console.log(idx)
+    const {id , item} = idx;
+    const findItems = shopItems.find((idx) => idx.id === id) || [];
+    
     return `
-    <div class="selectedCartItems">Loading...</div>
+    <div class="selectedCartItems">
+    <img width="100" src=${findItems.image}/>
+    <div class="details">
+    <div class="priceTitle">
+    <h4>
+    <p>${findItems.name}</p>
+    </h4>
+    </div>
+    <div class="cartButtons"></div>
+    <h3></h3>
+    </div>
+    </div>
     `
   }).join(""));
  } else {
@@ -128,3 +128,16 @@ let loadCartItems = () => {
 }
 
 loadCartItems();
+
+const itemsInCart = () => {
+  let cartIcon = document.getElementById("itemsUpdate");
+  cartIcon.innerHTML = cartBasket.map((idx) => idx.quantity).reduce((x, y) => x + y, 0);
+}
+
+const itemsMobileInCart = () => {
+  let cartIcon = document.getElementById("mobileItemsUpdate");
+  cartIcon.innerHTML = cartBasket.map((idx) => idx.quantity).reduce((x, y) => x + y, 0);
+}
+
+itemsInCart();
+itemsMobileInCart();
