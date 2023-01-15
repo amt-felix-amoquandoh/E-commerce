471ca5e6b95cf865d57c7c029b46cd913765d626
Felix Ekow Amoquandoh <105642899+felixEKow@users.noreply.github.com>
2023-01-15 14:47:33 +0000

    project files updated

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // import { shopItems } from "./shop.js";
 // shop variables
-const shop = document.getElementById("productsBox");
+const productArea = document.getElementById("productsBox");
 const cartButton = document.querySelector(".cartBox");
 const closeCartBtn = document.querySelector(".closeCart");
 const ClearCartBtn = document.querySelector(".cartFooterButton");
@@ -9,11 +9,86 @@ const cartOverlay = document.querySelector(".cartOverlay");
 const cartItemsQuantity = document.getElementById("mobileItemsUpdate itemsUpdate");
 const cartTotal = document.querySelector(".ItemsTotal");
 const overlayCartContent = document.querySelector(".overlayCartContent");
-const productArea = document.querySelector(".product");
 
 
 let cartBasket = [];
 
+// getting products implementation below
+class Products {
+  async getProducts(){
+    try {
+      let result = await fetch("shop.json");
+      let data = await result.json();
+      let products = data.items;
+      products = products.map(item => {
+        const {title, price, description} = item.fields;
+        const {id} = item.sys;
+        const image = item.fields.image.fields.file.url;
+        return {title, price, description, id, image};
+      })
+      return products;
+    } catch (error) {
+      console.log(error);      
+    }
+  }
+}
+
+// display products implementation
+class UI {
+  loadAllproducts(products){
+    let itemResult = "";
+    products.forEach(product => {
+      itemResult += `
+      <!-- single Product -->
+      <div class="product">
+        <img class="itemImage" src=${product.image} />
+        <div class="description">
+          <span class="itemTitle">${product.title}</span>
+          <h5>${product.description}</h5>
+          <div class="stars">
+            <ion-icon name="star"></ion-icon>
+            <ion-icon name="star"></ion-icon>
+            <ion-icon name="star"></ion-icon>
+            <ion-icon name="star"></ion-icon>
+            <ion-icon name="star"></ion-icon>
+          </div> 
+        </div>
+        <div class="priceBtns">
+          <h4 class="itemPrice">Ghc ${product.price}</h4>
+        <a><ion-icon name="cart-outline" class="proCart" data-id = ${product.id}></ion-icon></a>
+        <!--  -->
+        </div>
+      </div> 
+      <!-- single product ends here -->
+      `      
+    });
+    productArea.innerHTML = itemResult;
+  }
+  getAddToCartBtns(){
+    const addToCartButtons = [...document.querySelectorAll(".proCart")];
+  }
+}
+
+
+//saving cart items to local storage
+class Storage{
+   static saveCartItems (products){
+    localStorage.setItem("products", JSON.stringify(products));
+   }
+}
+
+document.addEventListener("DOMContentLoaded", ()=>{
+  const ui = new UI();
+  const products = new Products();
+
+  //get product items
+  products.getProducts().then(products => {
+    ui.loadAllproducts(products);
+    Storage.saveCartItems(products);
+  }).then( () => {
+    ui.getAddToCartBtns();
+  });
+})
 
 
 
