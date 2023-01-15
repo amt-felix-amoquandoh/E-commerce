9e820160a72221716ba42a1862e4423f22b5be9c
Felix Ekow Amoquandoh <105642899+felixEKow@users.noreply.github.com>
2023-01-15 13:29:04 +0000

    cart files update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-import { shopItems } from "./shop.js";
-//  Shop Items display
+// import { shopItems } from "./shop.js";
+// shop variables
 const shop = document.getElementById("productsBox");
 const cartButton = document.querySelector(".cartBox");
 const closeCartBtn = document.querySelector(".closeCart");
@@ -12,40 +12,84 @@ const overlayCartContent = document.querySelector(".overlayCartContent");
 const productArea = document.querySelector(".product");
 
 
-let cartBasket = JSON.parse(localStorage.getItem("suits")) || [];
+let cartBasket = [];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // items loading to DOM functions
-function loadItems() {
-  return (shop.innerHTML = shopItems
-    .map((item) => {
-      let { image, title, description, price, uniqueId } = item;
-      return `<div id=product-id-${uniqueId} class="product">
-    <img src="${image}" alt="" />
-    <div class="description">
-      <span>${title}</span>
-      <h5>${description}</h5>
-      <div class="stars">
-        <ion-icon name="star"></ion-icon>
-        <ion-icon name="star"></ion-icon>
-        <ion-icon name="star"></ion-icon>
-        <ion-icon name="star"></ion-icon>
-        <ion-icon name="star"></ion-icon>
-      </div>      
-    </div>
-    <div class="priceBtns">
-    <h4>${price}</h4>
-    <a><ion-icon name="cart-outline" class="proCart"></ion-icon></a>
-    </div>
-  </div>`;
-    })
-    .join(""));
-}
+// function loadItems() {
+//   return (shop.innerHTML = shopItems
+//     .map((item) => {
+//       let { image, title, description, price, uniqueId } = item;
+//       return `<div id=product-id-${uniqueId} class="product">
+//     <img src="${image}" alt="" />
+//     <div class="description">
+//       <span>${title}</span>
+//       <h5>${description}</h5>
+//       <div class="stars">
+//         <ion-icon name="star"></ion-icon>
+//         <ion-icon name="star"></ion-icon>
+//         <ion-icon name="star"></ion-icon>
+//         <ion-icon name="star"></ion-icon>
+//         <ion-icon name="star"></ion-icon>
+//       </div>      
+//     </div>
+//     <div class="priceBtns">
+//     <h4>${price}</h4>
+//     <a><ion-icon name="cart-outline" class="proCart"></ion-icon></a>
+//     </div>
+//   </div>`;
+//     })
+//     .join(""));
+// }
 
-const generateProducts = () => {
-  loadItems();
-};
-generateProducts();
+// const generateProducts = () => {
+//   loadItems();
+// };
+// generateProducts();
 
 
 
